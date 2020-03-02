52378178a10aeccd66caebbbec9e43dcaad75af8
Andrey <an256.ak@gmail.com>
2020-03-03 01:03:52 +0200

    created folder css & file style.css, wrote task css.min

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cssmin = require('gulp-cssmin');
+const rename = require("gulp-rename");
 
 gulp.task('hello', function(done) {
   console.log('Привет, мир!');
@@ -13,5 +15,15 @@ gulp.task('browser-sync', function() {
       baseDir: "./"
     }
   });
+  gulp.watch("./css/*.css").on('change', browserSync.reload);
   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// Cssmin
+gulp.task('cssmin', function (done) {
+  gulp.src('./css/*.css')
+    .pipe(cssmin())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./css/'));
+  done();
 });
