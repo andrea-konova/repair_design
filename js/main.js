e17842ab04f9d060a3ccf5d63a5c2be476337ff5
Andrey <an256.ak@gmail.com>
2020-03-12 03:06:38 +0200

    fix: test additional task

--- js/main.js
+++ js/main.js
@@ -12,3 +12,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
   closeBtn.addEventListener('click', switchModal);
 
 });
+
+/* document.addEventListener('keypress', function (e) {
+  const modal = document.querySelector('.modal');
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const closeBtn = document.querySelector('.modal__close');
+  if (e.keyCode === 27) document.getElementById('modal').hidden = 1;
+  
+}); */
+
+/* $(document).click(function (e) {
+  var elem = $('.modal'); 
+   if (e.target != elem[0] && !elem.has(e.target).length) { elem.hide();
+   } 
+  }) */
