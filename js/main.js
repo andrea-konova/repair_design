6478b879e89c2b7329ed38ba7514fb94989762ef
unknown <an256.ak@gmail.com>
2020-03-21 16:28:09 +0200

    fix: fixed form valadate

--- js/main.js
+++ js/main.js
@@ -83,6 +83,7 @@ $(document).ready(function () {
 // Валидация форм
 // control__form
 $('.control__form').validate ({
+  errorElement: "em",
   errorClass: "invalid",
   rules: { 
     // строчное правило {required:true}
@@ -91,7 +92,10 @@ $('.control__form').validate ({
       minlength: 2,
       maxlength: 15
     },
-    userPhone: "required"
+    userPhone: {
+      required: true,
+      minlength: 17
+    },
   },
   // правило сообщение
   messages: {
@@ -100,13 +104,17 @@ $('.control__form').validate ({
       minlength: "Имя не короче 2 букв",
       maxlength: "Имя не длиннее 15 букв"
     },
-    userPhone: "Заполните поле 'Телефон'"
+    userPhone: {
+      required: "Заполните поле 'Телефон'",
+      minlength: "Введите корректный телефон"
+    },
   }
 });
 // end control__form
 
 // modal__form
 $('.modal__form').validate({
+  errorElement: "em",
   errorClass: "invalid",
   rules: {
     // строчное правило {required:true}
@@ -115,7 +123,11 @@ $('.modal__form').validate({
       minlength: 2,
       maxlength: 15
     },
-    userPhone: "required",
+    userPhone: {
+      required: true,
+      minlength: 17
+    },
+    policyCheckbox: "required",
     // правило-объект (блок)
     userEmail: {
       required: true,
@@ -129,10 +141,13 @@ $('.modal__form').validate({
       minlength: "Имя не короче 2 букв",
       maxlength: "Имя не длиннее 15 букв"
     },
-    userPhone: "Заполните поле 'Телефон'",
+    userPhone: {
+      required: "Заполните поле 'Телефон'",
+      minlength: "Введите корректный телефон"
+    },
     userEmail: {
       required: "Заполните поле 'Email'",
-      email: "Введите корректный emailВведите корректный email"
+      email: "Введите корректный email"
     }
   }
 });
@@ -140,6 +155,7 @@ $('.modal__form').validate({
 
 // footer__form
 $('.footer__form').validate({
+  errorElement: "em",
   errorClass: "invalid",
   rules: {
     // строчное правило {required:true}
@@ -148,8 +164,14 @@ $('.footer__form').validate({
       minlength: 2,
       maxlength: 15
     },
-    userPhone: "required",
-    userQuestion: "required",
+    userPhone: {
+      required: true,
+      minlength: 17
+    },
+    userQuestion: {
+      required: true,
+      minlength: 10
+    },
     // правило-объект (блок)
     userEmail: {
       required: true,
@@ -163,11 +185,17 @@ $('.footer__form').validate({
       minlength: "Имя не короче 2 букв",
       maxlength: "Имя не длиннее 15 букв"
     },
-    userPhone: "Заполните поле 'Телефон'",
-    userQuestion: "Заполните поле 'Вопрос'",
+    userPhone: {
+      required: "Заполните поле 'Телефон'",
+      minlength: "Введите корректный телефон"
+    },
+    userQuestion: {
+      required: "Заполните поле 'Вопрос'",
+      minlength: "Вопрос не короче 10 символов",
+    },
     userEmail: {
       required: "Заполните поле Email",
-      email: "Введите корректный emailВведите корректный email"
+      email: "Введите корректный email"
     }
   }
 });
@@ -214,50 +242,3 @@ ymaps.ready(function () {
   map.behaviors
     .disable("scrollZoom");
 });
-
-// ! function (a) {
-//   a.fn.viewportChecker = function (b) {
-//     var c = {
-//       classToAdd: "visible",
-//       classToRemove: "invisible",
-//       offset: 100,
-//       repeat: !1,
-//       invertBottomOffset: !0,
-//       callbackFunction: function (a, b) {},
-//       scrollHorizontal: !1
-//     };
-//     a.extend(c, b);
-//     var d = this,
-//       e = {
-//         height: a(window).height(),
-//         width: a(window).width()
-//       },
-//       f = -1 != navigator.userAgent.toLowerCase().indexOf("webkit") ? "body" : "html";
-//     return this.checkElements = function () {
-//       var b, g;
-//       c.scrollHorizontal ? (b = a(f).scrollLeft(), g = b + e.width) : (b = a(f).scrollTop(), g = b + e.height), d.each(function () {
-//         var d = a(this),
-//           f = {},
-//           h = {};
-//         if (d.data("vp-add-class") && (h.classToAdd = d.data("vp-add-class")), d.data("vp-remove-class") && (h.classToRemove = d.data("vp-remove-class")), d.data("vp-offset") && (h.offset = d.data("vp-offset")), d.data("vp-repeat") && (h.repeat = d.data("vp-repeat")), d.data("vp-scrollHorizontal") && (h.scrollHorizontal = d.data("vp-scrollHorizontal")), d.data("vp-invertBottomOffset") && (h.scrollHorizontal = d.data("vp-invertBottomOffset")), a.extend(f, c), a.extend(f, h), !d.hasClass(f.classToAdd) || f.repeat) {
-//           String(f.offset).indexOf("%") > 0 && (f.offset = parseInt(f.offset) / 100 * e.height);
-//           var i = f.scrollHorizontal ? Math.round(d.offset().left) + f.offset : Math.round(d.offset().top) + f.offset,
-//             j = f.scrollHorizontal ? i + d.width() : i + d.height();
-//           f.invertBottomOffset && (j -= 2 * f.offset), g > i && j > b ? (d.removeClass(f.classToRemove), d.addClass(f.classToAdd), f.callbackFunction(d, "add")) : d.hasClass(f.classToAdd) && f.repeat && (d.removeClass(f.classToAdd), f.callbackFunction(d, "remove"))
-//         }
-//       })
-//     }, a(document).bind("touchmove MSPointerMove pointermove", this.checkElements), a(window).bind("load scroll touchmove", this.checkElements), a(window).resize(function (b) {
-//       e = {
-//         height: a(window).height(),
-//         width: a(window).width()
-//       }, d.checkElements()
-//     }), this.checkElements(), this
-//   }
-// }(jQuery);
-
-// jQuery(document).ready(function () {
-//   jQuery('.elementanm').addClass("hidden").viewportChecker({
-//     classToAdd: 'visible animated slideLeft',
-//     offset: 100
-//   });
-// });
