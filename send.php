caaffd6f32b6d8f3a0375f3aed8dc115d5ef6618
unknown <an256.ak@gmail.com>
2020-03-22 16:21:31 +0200

    feat: add ajax

--- send.php
+++ send.php
@@ -33,8 +33,14 @@ try {
     $mail->Subject = 'Новая заявка с сайта';
     $mail->Body    = "Имя пользователя: ${userName}, телефон: ${userPhone}. Почта: ${userEmail}, вопрос ${userQuestion}";
 
-    $mail->send();
-    header('Location: thanks.html');
+    if ($mail->send()) {
+        echo "ok";
+    } else {
+        echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+    }
+    
+    
+    
 } catch (Exception $e) {
-    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+    
 }
