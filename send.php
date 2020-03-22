c4839a37aade85db54360ee6b7e2961a5cae1179
unknown <an256.ak@gmail.com>
2020-03-22 14:41:44 +0200

    fix: fixed phpmailer

--- send.php
+++ send.php
@@ -3,11 +3,12 @@
 $userName = $_POST['userName'];
 $userEmail = $_POST['userEmail'];
 $userPhone = $_POST['userPhone'];
+$userQuestion = $_POST['userQuestion'];
 
 // Load Composer's autoloader
+require 'phpmailer/Exception.php';
 require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
-require 'phpmailer/Exception.php';
 
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer\PHPMailer\PHPMailer();
@@ -19,22 +20,21 @@ try {
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'andrii.konovalenko95@gmail.com';                     // SMTP username
-    $mail->Password   = 'hakimov13';                               // SMTP password
+    $mail->Password   = 'Timon256Nu';                               // SMTP password
     $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 465;                                    // TCP port to connect to
 
     //Recipients
     $mail->setFrom('andrii.konovalenko95@gmail.com', 'Андрей');
-    $mail->addAddress('anutina256@gmail.com');     // Add a recipient
+    $mail->addAddress('andrii.konovalenko@yandex.ru');     // Add a recipient
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Новая заявка с сайта';
-    $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}. Его почта: ${userEmail}";
+    $mail->Body    = "Имя пользователя: ${userName}, телефон: ${userPhone}. Почта: ${userEmail}, вопрос ${userQuestion}";
 
     $mail->send();
     header('Location: thanks.html');
 } catch (Exception $e) {
     echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
 }
-
